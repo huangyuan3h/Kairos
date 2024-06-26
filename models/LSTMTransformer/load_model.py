a263ac90685d2633f7e209dc8d0f71a9c39a9cc2
Yuan Huang <huangyuan3h@gmail.com>
2024-06-26 12:46:55 +0800

    update logic for training

--- models/LSTMTransformer/load_model.py
+++ models/LSTMTransformer/load_model.py
@@ -1,6 +1,17 @@
 import torch
-
+import os
 
 def load_model(model, load_path):
-    model.load_state_dict(torch.load(load_path))
+    """加载模型
+    Args:
+        model: 模型实例
+        load_path: 模型文件路径
+    Returns:
+        模型实例
+    """
+    if os.path.exists(load_path):
+        model.load_state_dict(torch.load(load_path))
+        print(f"模型已加载：{load_path}")
+    else:
+        print(f"模型文件不存在，将创建新模型：{load_path}")
     return model
--- src/training/training_lstm_transformer.py
+++ src/training/training_lstm_transformer.py
@@ -8,19 +8,21 @@ from models.LSTMTransformer.StockDataset import StockDataset
 import torch.nn as nn
 import torch.optim as optim
 
+from models.LSTMTransformer.load_model import load_model
 from models.LSTMTransformer.predict import predict
 from models.LSTMTransformer.train_model import train_model
+import os
 
 # 模型参数
 input_dim = 47  # 输入特征维度 (保持不变)
 hidden_dim = 128  # LSTM隐藏层维度，扩大一倍
-num_layers = 3   # LSTM层数，增加一层
-num_heads = 8   # Transformer注意力头数，扩大一倍
+num_layers = 3  # LSTM层数，增加一层
+num_heads = 8  # Transformer注意力头数，扩大一倍
 target_days = 10  # 预测未来天数 (保持不变)
 
 # 训练参数
-batch_size = 32 # 可以尝试稍微调大，例如 64 或 128，但要根据你的硬件条件
-learning_rate = 0.001 # 可以尝试稍微调小，例如 5e-4 或 1e-4
+batch_size = 32  # 可以尝试稍微调大，例如 64 或 128，但要根据你的硬件条件
+learning_rate = 0.00001  # 可以尝试稍微调小，例如 5e-4 或 1e-4
 num_epochs = 200  # 训练轮数增加
 model_save_path = "../../model_files/lstm_transformer_model.pth"
 
@@ -29,13 +31,19 @@ feature_columns = [i for i in range(input_dim)]  # 特征列索引
 target_column = 1  # 目标列索引
 
 
+# 接下来的训练次数
+next_training_batch = 10
+
 def main():
-    dataset = StockDataset(target_days, feature_columns, target_column)
-    data_loader = DataLoader(dataset, batch_size, shuffle=True)
     model = LSTMTransformerModel(input_dim, hidden_dim, num_layers, num_heads, target_days)
+    model = load_model(model, model_save_path)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    train_model(model, data_loader, criterion, optimizer, num_epochs, model_save_path)
+
+    for i in range(next_training_batch):
+        dataset = StockDataset(target_days, feature_columns, target_column)
+        data_loader = DataLoader(dataset, batch_size, shuffle=True)
+        train_model(model, data_loader, criterion, optimizer, num_epochs, model_save_path)
 
     data = get_random_valid_data()
     predict_data = data[0:60]
@@ -44,8 +52,7 @@ def main():
     scaler.fit(predict_data)
     predictions = predict(model, predict_data, scaler, feature_columns)
     print("未来10天的预测数据：", predictions)
-    print(expected_data)
-
+    print(expected_data["close_x"])
 
 
 if __name__ == "__main__":
