907f22632889062f47817d22ca9f0ffa1ddd9f6f
Yuan Huang <huangyuan3h@gmail.com>
2024-07-02 11:49:13 +0800

    add sz import

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ from data.raw import get_currency_exchange_rates, get_szse_component_index
 from db import create_table
 from import_2_db import import_exchange_rates
 from import_2_db.import_sh_index_daily import import_sh_index_daily
+from import_2_db.import_sz_index_daily import import_sz_index_daily
 
 start_date = '20190101'
 
 
 def main():
     create_table()
-    import_sh_index_daily()
+    import_sz_index_daily()
 
 
 if __name__ == "__main__":
