4c39e12338d75ff179a88d3167c5dc217f50386c
Yuan Huang <huangyuan3h@gmail.com>
2024-07-25 09:20:02 +0800

    it is the stable version of hybrid model

--- models/LSTMTransformer/config.py
+++ models/LSTMTransformer/config.py
@@ -3,7 +3,7 @@ from models.LSTMTransformer import LSTMAttentionTransformer
 config_lstm_transformer_modelV1 = {
     "input_dim": 73,
     "hidden_dim": 768,
-    "num_layers": 16,
+    "num_layers": 3,
     "num_heads": 32,
     "batch_size": 64,
     "learning_rate": 1e-3,
