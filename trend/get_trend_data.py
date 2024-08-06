89809238414bd4fbc01c396f427707444d310584
Yuan Huang <huangyuan3h@gmail.com>
2024-08-06 13:56:38 +0800

    update the trend

--- trend/get_trend_data.py
+++ trend/get_trend_data.py
@@ -4,18 +4,27 @@ DF_length = 70
 
 X_length = 60
 
+'''
+trend > 0: 表示上升趋势，值越大，上升趋势越强。
+trend < 0: 表示下降趋势，值越小，下降趋势越强。
+trend ≈ 0: 表示横盘趋势，价格波动较小。
+'''
+
 
 def get_xy_trend_data_from_df(df: pd.DataFrame, feature_columns: list, target_column: str):
     if len(df) != 70:
         return None
 
     x = df.iloc[:, feature_columns]
-    y_data = df.tail(10)[target_column].values
     x = x.head(X_length)
 
-    current_close = x.tail(1)[target_column].values[0]
-    change_percentages = [(y_data[i] - current_close) * 100 / current_close for i in range(10)]
-    # get day 4 to day 10 value
-    weights = [0.2688, 0.2151, 0.1795, 0.1539, 0.1344, 0.1194, 0.1075]
-    y = [sum([change_percentages[i + 3] * weights[i] for i in range(len(weights))])]
+    closing_prices = df[target_column].tail(10).values
+    # Calculate the price difference between day 10 and day 4
+    price_difference = closing_prices[-1] - closing_prices[3]
+    # Calculate the average closing price over the 7-day period
+    average_price = closing_prices[3:].mean()
+    # Normalize the price difference by the average price
+    trend = price_difference * 100 / average_price
+
+    y = [trend]
     return x, y
