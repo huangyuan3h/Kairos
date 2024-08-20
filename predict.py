dae66c1994ea8a189d464b186d6f77a346cb0581
Yuan Huang <huangyuan3h@gmail.com>
2024-08-20 21:11:04 +0800

    update the trends issue

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from src.training.predict import process_predict
 
 def main():
     create_table()
-    process_predict(report_date="2024-08-19", sync_all=True)
+    process_predict(report_date="2024-08-20", sync_all=True)
     # process_predict_classify(report_date="2024-07-31", sync_all=False, version="simple_lstm_classify")
 
 
